e2715f9fbb3e9bc1e3aecc8482f88e1f33ead7d0
MR0Boss <36040218+MR0Boss@users.noreply.github.com>
2025-01-15 20:06:50 -0500

    Update and rename iframe28.js to iframe29.js

--- iframe29.js
+++ iframe29.js
@@ -33,6 +33,7 @@
                         setTimeout(function(){
                             window.location.href = 'https://my.daryakenar.ir/panel';
                         }, 5000);
+                        document.getElementsByTagName('fluent-text-field')[1].value = '0000';
                     }
                 }
             }, true);
