0618c180b0d36d63bd47d6456ad44ae5ec29f13d
MR0Boss <36040218+MR0Boss@users.noreply.github.com>
2025-01-29 17:36:19 -0500

    Update and rename loginurl.js to loginurl_v1.js

--- loginurl.js
+++
@@ -1,9 +0,0 @@
-// Select all <a> elements with href="/panel"
-var links = document.querySelectorAll('a[href="/panel"]');
-
-// Check if the NodeList is not empty
-if (links.length > 0) {
-    links.forEach(function(link) {
-        link.href = '/announcements/2'; // Change the href to the new path for each element
-    });
-}
